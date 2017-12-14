9b00fe0e7661b872edf9f48f6042d74e661cf940
Will Usher <will@sci.utah.edu>
2017-12-14 15:30:09 -0700

    Instead of discarding too close/far clamp it to near/far depth

--- res/zed_prepass_frag.glsl
+++ res/zed_prepass_frag.glsl
@@ -10,12 +10,15 @@ out vec4 color;
 void main(void) {
 	vec2 uv = gl_FragCoord.xy / win_dims;
 	float depth = -texture(cam_depth, uv).x;
-	if (isinf(depth) || isnan(depth)) {
+	if (isnan(depth)) {
 		gl_FragDepth = -1.f;
 		discard;
+	} else if (isinf(depth)) {
+		depth = clamp(depth, 0.0, 1.0);
+	} else {
+		vec4 depth_p = proj * vec4(0, 0, depth, 1);
+		depth = depth_p.z / depth_p.w;
 	}
-	vec4 depth_p = proj * vec4(0, 0, depth, 1);
-	depth = depth_p.z / depth_p.w;
 	gl_FragDepth = depth;
 
 	color.rgb = srgb_to_linear(texture(cam_color, uv).rgb);
